7dde78e40443db959b0cd23707362ee6bbec65da
Johnny <johnny@example.com>
2020-02-05 22:13:30 +0100

    Completes aboutUs page

---
+++ about.php
@@ -0,0 +1,23 @@
+<?php include 'shared/header.php'; ?>
+<?php include 'shared/hiddenSidebar.php'; ?>
+<?php include 'staff/usedEquipmentData.php'; ?>
+
+<div class="content">
+  <div class="stripe"></div>
+  <h2>About Us</h2>
+  <div class="container">
+    <div class="row about p-4 p-lg-0">
+      <div class="col-lg-5 mb-5 mb-lg-0">
+        <p><span>IBP Termotehnika doo</span> is a family business with over 20 years of experience as a supplier in the heat treatment industry. We strive to provide our customers with the best possible solutions and equipment for their heat treatment needs. <br> With this goal in mind, we have become an official Ipsen International GmbH sales representative for Slovenia, Croatia, BiH, Serbia, Montenegro and Macedonia. Ipsen International has been the leading manufacturer of heat treatment equipment for decades. <br> We can provide you with new and used equipment, services, original spare parts, high-quality baskets, grids, stop off coatings and many other products related to this industry. <br> For all your needs we are just a phone call away, Your satisfaction is our priority!</p>
+      </div>
+      <div class="offset-lg-1"></div>
+      <div class="col-lg-6 mt-3 mt-lg-0 mb-4 mb-lg-0 aboutImage">
+        <img src="images/background.jpg" alt="" class="w-100">
+        <div class="contactFrame"><a href="contact.php">Contact Us</a></div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+<?php include 'shared/footer.php'; ?>
